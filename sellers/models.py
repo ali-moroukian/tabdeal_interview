388e21bf2ddcafd78b709deb141a10ecb2cc2a0d
moroukian <moroukianali@gmail.com>
2022-06-10 03:43:17 +0430

    feat(sellers): Add models

--- sellers/models.py
+++ sellers/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from sellers.choices import *
+
+
+class Seller(models.Model):
+    balance = models.PositiveBigIntegerField(default=0)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Transaction(models.Model):
+    TYPES = (
+        (TRANSACTION_TYPE_DEPOSIT, 'deposit'),
+        (TRANSACTION_TYPE_WITHDRAW, 'withdraw'),
+    )
+
+    amount = models.PositiveBigIntegerField()
+    type = models.CharField(max_length=50, choices=TYPES)
+    time = models.DateTimeField(auto_now_add=True)
