05c6a6648115ec55f80a5f82ec5fa9786138a7b3
moroukian <moroukianali@gmail.com>
2022-06-11 17:13:07 +0430

    feat(sellers): Add tests to check charge APIs

--- sellers/tests.py
+++ sellers/tests.py
@@ -1,3 +1,60 @@
+from django.db.models import Sum
+from django.test import Client
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from sellers.choices import TRANSACTION_TYPE_DEPOSIT, TRANSACTION_TYPE_WITHDRAW
+from sellers.models import Seller, Transaction
+
+
+class AnimalTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+        self.seller1 = Seller.objects.create()
+        self.seller2 = Seller.objects.create()
+
+    @staticmethod
+    def get_seller_balance(seller_id):
+        return Seller.objects.get(id=seller_id).balance
+
+    def test_seller_balance(self):
+        resp = self.client.post(
+            reverse('seller_charge', kwargs={'seller_id': self.seller1.id}),
+            data={'amount': 100}
+        )
+        self.assertEqual(resp.status_code, 200)
+
+        resp = self.client.post(
+            reverse('seller_charge', kwargs={'seller_id': self.seller1.id}),
+            data={'amount': 1000}
+        )
+        self.assertEqual(resp.status_code, 200)
+
+        resp = self.client.post(
+            reverse('seller_charge', kwargs={'seller_id': self.seller2.id}),
+            data={'amount': 1000}
+        )
+        self.assertEqual(resp.status_code, 200)
+
+        for _ in range(60):
+            resp = self.client.post(
+                reverse('phone_charge', kwargs={'seller_id': self.seller2.id}),
+                data={'amount': 5, 'phone_number': '09123456789'}
+            )
+            self.assertEqual(resp.status_code, 200)
+
+        resp = self.client.post(
+            reverse('phone_charge', kwargs={'seller_id': self.seller2.id}),
+            data={'amount': 1000, 'phone_number': '09123456789'}
+        )
+        self.assertEqual(resp.status_code, 400)
+
+        self.assertEqual(self.get_seller_balance(self.seller1.id), 1100)
+        self.assertEqual(self.get_seller_balance(self.seller2.id), 700)
+
+        deposit_amount = Transaction.objects.filter(
+            type=TRANSACTION_TYPE_DEPOSIT).aggregate(Sum('amount'))['amount__sum']
+        withdraw_amount = Transaction.objects.filter(
+            type=TRANSACTION_TYPE_WITHDRAW).aggregate(Sum('amount'))['amount__sum']
+        total_balance = Seller.objects.aggregate(Sum('balance'))['balance__sum']
+        self.assertEqual(deposit_amount, withdraw_amount + total_balance)
--- sellers/urls.py
+++ sellers/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from sellers.views import ChargeSellerAPIView, ChargePhoneAPIView
 
 urlpatterns = [
-    path('<int:seller_id>/charge/', ChargeSellerAPIView.as_view()),
-    path('<int:seller_id>/charge_phone/', ChargePhoneAPIView.as_view()),
+    path('<int:seller_id>/charge/', ChargeSellerAPIView.as_view(), name='seller_charge'),
+    path('<int:seller_id>/charge_phone/', ChargePhoneAPIView.as_view(), name='phone_charge'),
 ]
